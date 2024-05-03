271040b663780c88567cd60e3e6021fb567d2e9e
Robert Vásquez <vasquezrober09@gmail.com>
2024-05-03 14:55:57 -0400

    Media query desktop finished

---
+++ README.md
@@ -0,0 +1,2 @@
+# Responsive web js
+This project is a resposive web created with html, css and javascript vanilla
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,13 @@ const menu = document.querySelector('.nav__menu');
 const menuList = document.querySelector('.nav__list');
 const links = document.querySelectorAll('.nav__link');
 
+// Add class nav__list--show to menu
 menu.addEventListener('click', () => {
 	menuList.classList.toggle('nav__list--show');
 });
 
+
+// Remove nav__list--show to links
 links.forEach((link) => {
 	link.addEventListener('click', () => {
 		menuList.classList.remove('nav__list--show');
